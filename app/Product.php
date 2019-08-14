caac8e49524410d3fe8880a6454a52ef3ca96a48
Maksim Sheiko <maksim-sheiko@mail.com>
2019-08-14 19:46:44 +0300

    add category and tag

--- app/Product.php
+++ app/Product.php
@@ -4,6 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Product
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Product query()
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
     //
---
+++ app/Tag.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Tag
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag query()
+ * @mixin \Eloquent
+ */
+class Tag extends Model
+{
+    //
+}
